c5c23fccda56fd9fdbc432bb785ae16d4ee988a8
D.M Arbaaz <arbaaz2410@gmail.com>
2018-04-24 22:07:12 +0530

    Add files via upload
    V2

---
+++ v2/getPathNames.php
@@ -0,0 +1,47 @@
+<?php
+require_once "config.php";
+$return = array();
+function scanFolders($dir){
+    global $exclude_dirs;
+    $dirs = array();
+
+    if(!is_readable($dir)){
+        return $dirs;
+    }
+    $dir = 'C:/xampp/htdocs/FS Project/fs/library/My Music/';
+    $myfile = fopen("track_paths.txt", "w") or die("Unable to open file!");
+    $list = scandir($dir);
+    $i=0;
+    foreach($list as $value){
+        if(!($i==0 || $i==1)) {
+            fwrite($myfile, $dir.$value.PHP_EOL);
+        }
+        $i++;
+    }
+    fclose($myfile);
+    /*
+    foreach($list as $row){
+        if(is_dir($dir.$row) && !in_array($row, $exclude_dirs)){
+            $dirs[$row] = array(
+                "name" => $row,
+                "children" => scanFolders($dir . $row."/")
+            );
+        }
+    }
+    return $dirs;
+    //echo "<pre>".print_r($list, true)."</pre>";
+    */
+}
+$dirs = scanFolders($library);
+/*
+if($dynamic_dir_scan === true){
+    echo json_encode($dirs);
+}else{
+    if(is_writable($root."public/cache/directories.json")){
+        $file_handler = fopen($root."public/cache/directories.json", "w+");
+        fwrite($file_handler, json_encode($dirs));
+        fclose($file_handler);
+    }
+}
+*/
+?>
---
+++ v2/set_global_var.php
@@ -0,0 +1,8 @@
+<?php
+$_POST['sort_by']="album";
+$_POST['name']="";
+$_POST['submittype']='SORT';
+$_POST['submit']='fdsf';
+unset($_POST['submit']);
+include 'index.php';
+?>

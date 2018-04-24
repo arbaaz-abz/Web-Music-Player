d23a05f2f1af12e7a104d94d9c85731a4a81d9f2
D.M Arbaaz <arbaaz2410@gmail.com>
2018-04-25 01:26:36 +0530

    Update set_global_var.php

--- v2/set_global_var.php
+++ v2/set_global_var.php
@@ -3,6 +3,8 @@ $_POST['sort_by']="album";
 $_POST['name']="";
 $_POST['submittype']='SORT';
 $_POST['submit']='fdsf';
+session_start();
+$_SESSION['playlist'] = array();
 unset($_POST['submit']);
 include 'index.php';
 ?>
